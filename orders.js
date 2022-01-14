ed11ce5a5e011cf869d578ecf7353963e05e85cf
Anthony <adominguez15722@gmail.com>
2022-01-14 11:35:08 -0600

    Problem 1 is solved

--- orders.js
+++ orders.js
@@ -31,6 +31,19 @@
 
 //CODE HERE
 
+class Ticket {
+    constructor(items, orderTime, customerId, status){
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = 'queued'
+    }
+    updateStatus(newStatus){
+        this.status = newStatus
+        console.log(`The order for customer ${this.customerId} is now ${this.status}` )
+    }
+}
+
 
 
 /*
@@ -46,6 +59,9 @@
 
 //CODE HERE
 
+let firstTicket = new Ticket('pizz, bread, and soda', '7:03PM', 575)
+
+console.log(firstTicket);
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +70,5 @@
 */
 
 //CODE HERE
+
+firstTicket.updateStatus('cooking');
